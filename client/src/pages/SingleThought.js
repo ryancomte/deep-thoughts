f2c5467fa4681bc580773148f6e6a5ec742e5e99
Ryan Comte <ryancomte@gmail.com>
2021-09-02 14:14:16 -0600

    Added router features for multiple pages

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,19 +1,40 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_THOUGHT } from '../utils/queries';
 
-const SingleThought = props => {
+import { useParams } from 'react-router-dom';
+
+import ReactionList from '../components/ReactionList';
+
+const SingleThought = (props) => {
+  const { id: thoughtId } = useParams();
+
+  const { loading, data } = useQuery(QUERY_THOUGHT, {
+    variables: { id: thoughtId },
+  });
+
+  const thought = data?.thought || {};
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
+
   return (
     <div>
       <div className="card mb-3">
         <p className="card-header">
           <span style={{ fontWeight: 700 }} className="text-light">
-            Username
+            {thought.username}
           </span>{' '}
-          thought on createdAt
+          {thought.createdAt}
         </p>
         <div className="card-body">
-          <p>Thought Text</p>
+          <p>{thought.thoughtText}</p>
         </div>
       </div>
+      {thought.reactionCount > 0 && (
+        <ReactionList reactions={thought.reactions} />
+      )}
     </div>
   );
 };
