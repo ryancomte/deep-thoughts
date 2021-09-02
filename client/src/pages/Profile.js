c301aa366a51be74addc5c7ce46bce321d1eaa01
Ryan Comte <ryancomte@gmail.com>
2021-09-02 16:27:40 -0600

    Adding stuff
    Signed-off-by: Ryan Comte <ryancomte@gmail.com>

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Redirect, useParams } from 'react-router-dom';
 
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
 
 import { useQuery } from '@apollo/react-hooks';
-import { QUERY_USER } from '../utils/queries';
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import Auth from '../utils/auth';
 
 const Profile = () => {
   const { username: userParam } = useParams();
-  const { loading, data } = useQuery(QUERY_USER, {
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam },
   });
 
-  const user = data?.user || {};
+  const user = data?.me || data?.user || {};
 
   if (loading) {
     return <div>loading...</div>;
+  }
+  console.log('AUTH', Auth.getProfile().data.username);
+  if (
+    Auth.loggedIn() &&
+    Auth.getProfile().data?.username.toLowerCase() === userParam?.toLowerCase()
+  ) {
+    return <Redirect to="/profile" />;
   }
 
   return (
